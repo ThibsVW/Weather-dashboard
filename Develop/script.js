b56611f2400fbc4043ea2a06a9cfce7777a4fbb4
thibs <thibault.vanweydeveld@hotmail.com>
2020-06-15 14:40:32 +0800

    Ajax code

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,8 @@
 
 $(document).ready(function() {
 
-const searchCity = $("#searchCity");
-const runSearch = $("#run-search");
-const clearSearch = $("#clear-all");
+const searchCity = $("#search-term");
+const btnClear = $("#clear-all");
 const notificationElement = $("#notification");
 const iconElement =$('#weather-icons');
 const tempElement = $("#temperature-value p");
@@ -11,12 +10,23 @@ const descElement = $('#temperature-description p');
 const locationElement = $("#location p");
 const city = $("#searchCity").val();
 
-var queryURL =  "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ba2a1569518951bccaf8df7ccbbf0e7b";
-
 
 // .on("click") function associated with the search button
-$("#run-search").on("click", function(event) {
+$("#btnSearch").on("click", function(event) {
     event.preventDefault();
+
+
+var queryURL =  "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ba2a1569518951bccaf8df7ccbbf0e7b";
+
+$.ajax ({
+    URL : queryURL,
+    method :"GET"
+}).then(function(response){
+
+   console.log(response)
+
+})
+
    
    });
 
@@ -39,7 +49,6 @@ function clear (){
 
 //.on("click") function ssociated with the clear button
 $("#clear-all").on("click", clear);
-
 
 
 
