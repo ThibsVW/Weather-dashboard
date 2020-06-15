b2af87f071990f5e066770024816471fd0a14367
thibs <thibault.vanweydeveld@hotmail.com>
2020-06-15 13:34:35 +0800

    script built

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,43 @@
 $(document).ready(function() {
 
 const searchCity = $("#searchCity");
-const numberOfDays = $("#numberOfDays");
-const startDay = $("start");
-const endDay = $("end");
 const runSearch = $("#run-search");
 const clearSearch = $("#clear-all");
+const notificationElement = $("#notification");
+const iconElement =$('#weather-icons');
+const tempElement = $("#temperature-value p");
+const descElement = $('#temperature-description p');
+const locationElement = $("#location p");
+const city = $("#searchCity").val();
+
+var queryURL =  "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=ba2a1569518951bccaf8df7ccbbf0e7b";
+
+
+// .on("click") function associated with the search button
+$("#run-search").on("click", function(event) {
+    event.preventDefault();
+   
+   });
+
+
+
+
+//Takes API data (JSON object) and turns it into elements on the page
+function upDateWeather (){
+
+}
+
+// function to empty out previous researches
+function clear (){
+    $("#weather-result").empty();
+
+}
+
+
+
+
+//.on("click") function ssociated with the clear button
+$("#clear-all").on("click", clear);
 
 
 
