dccdf32f345248b925ceae606b65923074bd187b
thibs <thibault.vanweydeveld@hotmail.com>
2020-06-15 12:04:05 +0800

    html built

---
+++ Develop/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+
+const searchCity = $("#searchCity");
+const numberOfDays = $("#numberOfDays");
+const startDay = $("start");
+const endDay = $("end");
+const runSearch = $("#run-search");
+const clearSearch = $("#clear-all");
+
+
+
+
+})
