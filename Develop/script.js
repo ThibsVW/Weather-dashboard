4dbbbc11a4f06c4f7ceb0f17cd1618583a1bee55
thibs <thibault.vanweydeveld@hotmail.com>
2020-06-19 20:33:37 +0800

    data showing

--- Develop/script.js
+++ Develop/script.js
@@ -11,13 +11,25 @@ function citySearch() {
         
     }).then(function (response) {
         console.log(response);
+        const weather = response;
+        console.log(weather.name);
+        console.log(weather.main.temp);
+        console.log(weather.main.humidity);
+        console.log(weather.wind.speed);
+
+        const cityName = weather.name;
+        const cityTemp = weather.main.temp;
+        const cityHumidity = weather.main.humidity;
+        const cityWindSpeed = weather.wind.speed;
+
+        $("#city-name").text(cityName);
+        $("#city-temp").text("Temperature" + cityTemp.toFixed(1) +"C");
+        $("#city-humidity").text("humidity" + cityHumidity + "%");
+        $("#city-wind").text("wind-speed" + cityWindSpeed.toFixed(2)+ "KPH");
     });
 };
     //button to run search
 $("#btnSearch").on("click", citySearch)
-
-
-
 
 
 //Keep city searached by the guests
@@ -56,13 +68,11 @@ function init () {
         keepCities = storedCityNames;
     }
     renderCityNames();
-
 }
 
 function storeCityNames (){
     localStorage.setItem("keepCities", JSON.stringify(keepCities));
 }
-
 
 searchButton.on("click", function (event){
     event.preventDefault();
@@ -78,3 +88,36 @@ renderCityNames();
 
 })
 
+function uvIndex(lon, lat) {
+
+    var uvIndexURL = "https://api.openweathermap.org/data/2.5/uvi?appid=a2a1569518951bccaf8df7ccbbf0e7b&lat=" + lat + "&lon=" + lon + "&cnt=1";
+
+    $.ajax({
+        url : uvIndexURL,
+        method : "GET"
+    }).then(function(response){
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+
+        var uvIndFinal = response.value;
+        console.log(uvIndexURL);
+        if (uvIndFinal <= 2) {
+			// If LON&LAT is 2 or less, make Green
+			uvBtn.attr("class", "uvGreen");
+		} else if (uvIndFinal <=5) {
+			// If LON&LAT is 5 or less but greater than 2, make Yellow
+			uvBtn.attr("class", "uvYellow");
+		} else if (uvIndFinal <=7) {
+			// If LON&LAT is 7 or less but greater than 5, make Orange
+			uvBtn.attr("class", "uvOrange");
+		} else if (uvIndFinal < 11) {
+			// If LON&LAT is 10 or less but greater than 7, make Red
+			uvBtn.attr("class", "uvRed");
+		} else {
+			// If LON&LAT greater than 11, make Purple
+			uvBtn.attr("class", "uvPurple");
+		}
+
+    });
+
+}
